7abb52c43245f1e9345322f8f38a42fa1fe9ba93
Druce Vertes <drucev@gmail.com>
2024-05-26 13:53:49 -0400

    remove alt prompt

--- ainb_const.py
+++ ainb_const.py
@@ -23,49 +23,6 @@ MINTITLELEN = 28
 
 MAXPAGELEN = 50
 
-PROMPT = """
-Please serve as a research assistant for the purpose of categorizing news articles based on their relevance to artificial intelligence (AI).
-Your main responsibility will involve processing and classifying news articles formatted as JSON objects.
-
-Classification Criteria: Based on the title of each story, you are to classify whether the story primarily pertains to AI or not.
-Consider AI-related content to broadly include topics such as machine learning, supervised learning, unsupervised learning,
-reinforcement learning, robotics, computer vision, large language models, related topics, and specific references
-to AI entities like OpenAI, ChatGPT, Anthropic Claude, Google Gemini, Copilot, Perplexity.ai, Midjourney, etc.
-
-Input Specification: You will receive a list of news stories formatted as JSON objects separated by the delimiter "|".
-Each object includes an 'id' and a 'title'. For instance:
-|
-{'stories':
-[{'id': 97, 'title': 'AI to predict dementia, detect cancer'},
- {'id': 103,'title': 'Figure robot learns to make coffee by watching humans for 10 hours'},
- {'id': 103,'title': 'Baby trapped in refrigerator eats own foot'},
- {'id': 210,'title': 'ChatGPT removes, then reinstates a summarization assistant without explanation.'},
- {'id': 298,'title': 'The 5 most interesting PC monitors from CES 2024'},
- ]
-}
-|
-
-Output Specification: For each story, your output should be a JSON object containing the original 'id' and a new field 'isAI',
-which is a boolean indicating if the story is about AI. This output should be enclosed in the delimiter "~".
-The output schema must be strictly adhered to, without any additional fields. Example output:
-~
-{'stories':
-[{'id': 97, 'isAI': true},
- {'id': 103, 'isAI': true},
- {'id': 103, 'isAI': false},
- {'id': 210, 'isAI': true},
- {'id': 298, 'isAI': false}]
-}
-~
-
-Strictly ensure that each output object accurately reflects the corresponding input object in terms of the 'id' field
-and that the 'isAI' field accurately represents the AI relevance of the story as determined by the title.
-
-The list of news stories to classify and enrich is:
-
-
-"""
-
 bb_agent_system_prompt = """
 Role: You are an AI stock market assistant tasked with providing investors
 with up-to-date, detailed information on individual stocks.
